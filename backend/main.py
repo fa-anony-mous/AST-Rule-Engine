dcf2795854c13a1fe907f12d792ef4d1a3b0878c
Saketh Ram <sakethram9999@gmail.com>
2025-03-10 21:38:21 +0530

    changes in main.py

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,8 @@ import sys
 print(f"Python version: {sys.version}")
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.v1.router import api_router
-from app.core.settings import settings 
+from .app.api.v1.router import api_router
+from .app.core.settings import settings 
 
 
 app = FastAPI(
