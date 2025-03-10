aa1464323dd1129fe5772ef24fb1c6df1b815816
Saketh Ram <sakethram9999@gmail.com>
2025-03-10 22:12:57 +0530

    changed it back

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -2,7 +2,6 @@ import os
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     # Project Info
     PROJECT_NAME: str = "AST Rule Engine"
@@ -14,6 +13,7 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     
     # Database URL
+    POSTGRES_URL: str = "postgresql+asyncpg://postgres:saketh123@db.zfcqnzovzfgdogwlkzsj.supabase.co:5432/postgres?sslmode=require"
     SQLALCHEMY_DATABASE_URI: str
         
     # Supabase settings
@@ -31,8 +31,5 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-print(f"Loading .env file: {os.path.exists('.env')}")  # Check if .env exists
-print(f"SQLALCHEMY_DATABASE_URI from env: {os.environ.get('SQLALCHEMY_DATABASE_URI')}")  # Check env variable directly
-print(f"SQLALCHEMY_DATABASE_URI from settings: {settings.SQLALCHEMY_DATABASE_URI}")
 
     
